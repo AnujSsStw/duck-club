9d30cea68033decc824b3122a0597af23d552280
anuj <anujzzztw@gmail.com>
2024-10-21 02:55:45 +0530

    lgd

---
+++ web/src/components/map/map.tsx
@@ -0,0 +1,93 @@
+"use client";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  APIProvider,
+  ControlPosition,
+  Map,
+  MapControl,
+  Marker,
+} from "@vis.gl/react-google-maps";
+import React from "react";
+import { CurrentLocation } from "./current-location";
+import { PlaceAutocompleteClassic } from "./search-place";
+
+export default function MapComp({ form }: { form?: any }) {
+  const [userLocation, setUserLocation] = React.useState({ lat: 0, lng: 0 });
+
+  return (
+    <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY!}>
+      {form && (
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Select location</FormLabel>
+              <FormControl>
+                <>
+                  <Map
+                    style={{ width: "auto", height: "auto" }}
+                    className="w-full h-96"
+                    defaultCenter={{ lat: 22.54992, lng: 0 }}
+                    defaultZoom={3}
+                    gestureHandling={"greedy"}
+                    disableDefaultUI={true}
+                    onClick={(e) => {
+                      if (e.detail.latLng) {
+                        field.onChange(e.detail.latLng);
+                        setUserLocation(e.detail.latLng);
+                      }
+                    }}
+                  />
+                  {/* <AdvancedMarker position={userLocation} /> */}
+                </>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      )}
+
+      {userLocation.lat !== 0 && userLocation.lng !== 0 && (
+        <Marker position={userLocation} />
+      )}
+      {/* <InputMarker p={userLocation} /> */}
+      <MapControl position={ControlPosition.TOP_CENTER}>
+        <PlaceAutocompleteClassic
+          onPlaceSelect={(place) => {
+            console.log("place", place);
+            // setUserMarker({
+            //   lat: place?.geometry?.location?.lat(),
+            //   lng: place?.geometry?.location?.lng(),
+            // });
+          }}
+        />
+      </MapControl>
+      <MapControl position={ControlPosition.RIGHT_BOTTOM}>
+        <CurrentLocation />
+      </MapControl>
+      {/* <button
+        onClick={() => {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+              setUserLocation({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              });
+            });
+          } else {
+            // code for legacy browsers
+          }
+        }}
+      >
+        onclik
+      </button> */}
+    </APIProvider>
+  );
+}
